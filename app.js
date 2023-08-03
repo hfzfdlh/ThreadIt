24005d78361648b0eacd2c941b88afcf1797115c
danisa21 <danisaherviani21@gmail.com>
2023-08-03 16:40:16 +0700

    views done

--- app.js
+++ app.js
@@ -38,6 +38,26 @@ app.get('/home', (req, res) => {
     res.render('home');
 });
 
+// Tes page register
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+// Tes page profiles
+app.get('/profile', (req, res) => {
+    res.render('profile');
+});
+
+// Tes edit profiles
+app.get('/edit-profile', (req, res) => {
+    res.render('edit-profile');
+});
+
+// Tes page threads
+app.get('/thread', (req, res) => {
+    res.render('thread');
+});
+
 // Start the server
 app.listen(3000, () => {
     console.log('Server started on port 3000');
